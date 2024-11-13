cee556b1b7a134a4748e98dd2c72633e2c7c5bb9
LNagaraj <GH01361146@devcorptenant.com>
2024-11-13 15:16:28 -0500

    improved prompts

--- src/main/kotlin/com/neon/data/ClauseAnalysisResponse.kt
+++ src/main/kotlin/com/neon/data/ClauseAnalysisResponse.kt
@@ -2,6 +2,7 @@ package com.neon.data
 
 
 data class ClauseAnalysisResponse(
+    val clause: String,
     val summary: String,
     val risk_score: String,
     val reason: String,
--- src/main/kotlin/com/neon/data/ClausePrompt.kt
+++ src/main/kotlin/com/neon/data/ClausePrompt.kt
@@ -9,12 +9,12 @@ data class ClausePrompt(
 fun createClausePrompts(): Map<String, ClausePrompt> {
     return mapOf(
             "payment_terms" to ClausePrompt(
-                clause = "payment terms",
+                clause = "Payment Terms",
                 prompt ="""
 You are an expert legal assistant trained to analyze contracts.
 Task:
 1. Given the attached contract data, your goal is to analyze all clauses related to payment terms. If no clauses founds then return an empty JSON object as described below.
-2. Output a JSON object with the following fields:
+2. Output a JSON object with the following fields. It is critical to adhere to this schema in all cases. DO NOT include ```json``` in your response.
     - "summary": "Summarize the payment terms clauses in a concise manner, highlighting the key obligations or actions involved. If no payment terms are found, state that clearly."
     - "risk_score": "Very High, High, Medium, Low". Assign one single risk score to the clause based on the following criteria:
         - Very High: The clause is missing, or it contains vague language that could lead to disputes.
@@ -26,28 +26,28 @@ Task:
     
 ** Strict Format: Provide a JSON response like this. No additional commentary or explanations. **
 
-```json
   {
+  "clause": "payment terms",
   "summary": "The payment terms specify that the buyer shall pay the seller within 30 days of receiving the invoice. Payments shall be made by bank transfer, and late payments will incur an interest charge of 2% per month. The invoice must be submitted within 10 days after the completion of the service.",
   "risk_score": "Low",
   "reason": "The payment terms are clear, with specific timelines for payment, invoicing procedures, and penalties for late payments.",
   "missing_terms": []
   }
-```  
+ 
 END_OF_JSON
 
 
 """.trimIndent(),
                 weight = 1,
 
-            ),
+            ) ,
         "termination_clause" to ClausePrompt(
             clause = "termination clause",
             prompt = """
 You are an expert legal assistant trained to analyze contracts.
 Task:
-1 Given the attached contract data your goal is to analyze all clauses related to termination clause. If no clauses founds then return an empty JSON object as described below.
-2 Output JSON object with the following fields:
+1 Given the attached contract data your goal is to analyze all clauses related to termination clause. If no clauses found then return an empty JSON object as described below.
+2 Output JSON object with the following fields. It is critical to adhere to this schema in all cases. DO NOT include ```json``` in your response.
   - "summary": "Summarize the termination clause clauses in concise manner, highlighting the key obligations or actions involved. If no termination clause found, state that clearly."
   - "risk_score": "Very High, High, Medium, Low". Assign one single risk score to the clause based on the following criteria:
     - Very High: The clause is missing, or it contains vague language that could lead to disputes.
@@ -59,14 +59,14 @@ Task:
 
 ** Strict Format: Provide a JSON response like this. No additional commentary or explanations **
 
-```json
   {
+  "clause": "Termination Terms",
   "summary": "Either party may terminate this Agreement for convenience, without penalty, by providing a written notice at least 60 days prior to the intended termination date. During this notice period, both parties agree to fulfill all existing obligations, including any outstanding orders and payment commitments. Termination under this clause does not affect any accrued rights or obligations prior to the termination date.",
   "risk_score": "Low",
   "reason": "The termination clause is clear and balanced, with specific notice period and obligations for both parties.",
   "missing_terms": []
   }
-```
+  
 END_OF_JSON
 """.trimIndent(),
             weight = 1
